8f484320bb2b18b2d5229ff02cac7ead483c99f6
Gabriel de Laboulaye <gabriel.delaboulaye@qonto.com>
2024-12-05 09:39:16 +0100

    Remove useless code

--- 5_2.py
+++ 5_2.py
@@ -7,31 +7,23 @@ def fix_item(item):
             return fix_item(item)
     return item
 
-data = []
 with open("data/5.txt", "r") as file:
     data = file.read().split("\n\n")
-    conditions = data[0]
-    printings = data[1]
 
 print_conditions = []
-for i in conditions.split("\n"):
+for i in data[0].split("\n"):
     print_conditions.append([int(x) for x in i.split("|")])
 printed = []
-for i in printings.split("\n"):
+for i in data[1].split("\n"):
     printed.append([int(x) for x in i.split(",")])
 
 to_sum = []
 for printed_item in printed:
-    correct = True
     for condition in print_conditions:
         if condition[0] in printed_item and condition[1] in printed_item \
             and printed_item.index(condition[0]) > printed_item.index(condition[1]):
-            correct = False
             corrected_item = fix_item(printed_item)
-            print(corrected_item)
             to_sum.append(corrected_item[int(len(corrected_item)/2)])
-    if correct:
-        continue
 
 print(to_sum)
 print(sum(to_sum))
