3e572e21013042d1b5572b7f15379976ade30cfc
Gabriel de Laboulaye <gabriel.delaboulaye@qonto.com>
2024-12-08 09:07:24 +0100

    Day 8

--- 7_2.py
+++ 7_2.py
@@ -1,3 +1,5 @@
+import time
+
 def is_doable(tosum, integers, leftover=None):
     if leftover and leftover > tosum:
         return False
@@ -16,8 +18,10 @@ with open("data/7.txt", "r") as file:
         tosum, integers = int(line.strip().split(":")[0]), [int(i) for i in line.strip().split(":")[1].strip().split(" ")]
         data.append((tosum, integers))
 
+start = time.time()
 total = 0
 for tosum, integers in data:
     if is_doable(tosum, integers):
         total += tosum
 print(total)
+print(time.time() - start)
